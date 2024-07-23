8174b615731accc10939b4dbbdc8953c52a85383
observedobserver <270001151@qq.com>
2024-07-23 15:18:41 +0800

    feat: init meta data

---
+++ packages/cli/src/utils/meta-data.ts
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Landing page build with ConvertFast UI",
+  keywords: ["Landing Page", "ConvertFast-UI", "TailwindCSS"],
+  metadataBase: new URL('https://convertfa.st'),
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: '/og-image.png',
+  },
+};
--- packages/cli/src/utils/templates.ts
+++ packages/cli/src/utils/templates.ts
@@ -1,7 +1,10 @@
 import { ISegment } from "../interfaces.ts";
-
+import { metadata } from "./meta-data.ts"
+// TODO: different templates for different frameworks
 export function getTemplatePageCode(segments: ISegment[]) {
   return `${segments.map((seg) => `import { ${seg.name} } from './${seg.file}'`).join("\n")}
+
+export const metadata = ${JSON.stringify(metadata)}
   
 function LandingPage() {
   return (
