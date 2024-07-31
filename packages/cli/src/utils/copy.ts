4723a4a8a0102ec440d695e6543a7edc89f62428
observedobserver <270001151@qq.com>
2024-07-31 15:12:20 +0800

    fix: cross platform command for copy

---
+++ packages/cli/src/utils/copy.ts
@@ -0,0 +1,18 @@
+import fs from "fs/promises";
+import path from "path";
+
+export async function copyDir(src: string, dest: string) {
+  await fs.mkdir(dest, { recursive: true });
+  const entries = await fs.readdir(src, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await fs.copyFile(srcPath, destPath);
+    }
+  }
+}
